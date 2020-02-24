690923654fd0f354623a0a34f55dec7833ae904f
nakanishi-ryotaro <setthesunset25002000@gmail.com>
2020-02-24 10:22:47 +0900

    ScriptTest
    配列の宣言

---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = {10,1,2,3,4};
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[4-i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
